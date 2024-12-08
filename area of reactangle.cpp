543ebbde32abd2abf1af0bcf31202f2c2df2ae36
sushil2giri <girisushil2023@gmail.com>
2024-12-08 13:44:43 +0530

    Add files via upload

---
+++ area of reactangle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int length,width,area;
+	printf("Enter length and width");
+	scanf("%d %d,&length &width");
+	area=length*width;
+	printf("area of rectangle:%d\n",area);
+	return 0;
+	}
